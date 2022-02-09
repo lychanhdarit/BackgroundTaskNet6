d0287516ba81b7069cb5079cb35b4066ce35c90c
Da ric <lychanhdaric@gmail.com>
2022-02-09 11:56:21 +0700

    up

--- BackgroundTaskNet6/BackgroundTaskNet6/Jobs/DemoJobs.cs
+++ BackgroundTaskNet6/BackgroundTaskNet6/Jobs/DemoJobs.cs
@@ -5,10 +5,24 @@
         public static void Print()
         {
             Console.WriteLine($"Run Job: {DateTime.Now} ...");
+            //Write to a file
+            string something = File.ReadAllText("Log.txt");
+            using (StreamWriter writer = new StreamWriter("Log.txt"))
+            {
+                something += $"Run Job: {DateTime.Now} ...\n";
+                writer.WriteLine(something);
+            }
         }
         public static void Print2()
         {
             Console.WriteLine($"Run Job 2: {DateTime.Now} ...");
+            //Write to a file
+            string something = File.ReadAllText("Log.txt");
+            using (StreamWriter writer = new StreamWriter("Log.txt"))
+            {
+                something += $"Run Job 2: {DateTime.Now} ...\n";
+                writer.WriteLine(something);
+            }
         }
     }
 }
